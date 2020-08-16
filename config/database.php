d63889408403c94cea0b8abf6269454a09113811
10836014 <10836014@ntub.edu.tw>
2020-08-16 13:13:23 +0800

    更新資料庫連接(連heroku上的clearDB)

--- config/database.php
+++ config/database.php
@@ -2,12 +2,13 @@
 class Database{
  
     // specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "chatbot";
-    private $username = "root";
-    private $password = "mysqlpassword";
+    private $host = "us-cdbr-east-02.cleardb.com";
+    private $db_name = "heroku_5e33d8f743b0bb4";
+    private $username = "bb2a181ef54d3c";
+    private $password = "4b8d604d";
     public $conn;
  
+
     // get the database connection
     public function getConnection(){
  
